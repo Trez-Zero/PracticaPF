f2333dca52ee6cd9027a282afd32f8195549a722
Ric <ruizricardogame@gmail.com>
2026-05-20 12:07:18 -0600

    feat: configure logica

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿//Leer 25 edades y realizar lo siguiente, 1. Ingresar edades, 2. mostrar edades, 3. Mostrar mayores de edad, 4. mostrar menores de edad.
 
+int[] edades = new int[25];
+
 //1. Ingresar edades
+
 
 
 
