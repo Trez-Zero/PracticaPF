71530bbb0ce91c851da6009b7ca03d0e35e96f31
Ric <ruizricardogame@gmail.com>
2026-05-20 11:38:07 -0600

    First commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Leer 25 edades y realizar lo siguiente, 1. Ingresar edades, 2. mostrar edades, 3. Mostrar mayores de edad, 4. mostrar menores de edad.
+
+//1. Ingresar edades
+
+
+
+//2. mostrar edades
+
+
+
+//3. Mostrar mayores de edad
+
+
+
+//4. mostrar menores de edad
+
+
+
