29df2f2a5be3e197caba3e35d3a2df76c059f97f
Sergio <sicabrera@uamv.edu.ni>
2026-05-20 12:33:05 -0600

    feat: agregue mostrar  mayores de edad

--- Program.cs
+++ Program.cs
@@ -12,8 +12,14 @@ int[] edades = new int[25];
 
 
 //3. Mostrar mayores de edad
-
-
+Console.WriteLine("Mayores de edad: ");
+for (int i=0; i < edades.Length; i++)
+{
+    if (edades[i] >= 18)
+    {
+        Console.WriteLine(edades[i]);
+    }
+}
 
 //4. mostrar menores de edad
 
